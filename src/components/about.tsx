0f085644be41d97db916fbca1a4451bee225e60d
Hussam Qannam <hussam.a.qannam@gmail.com>
2025-05-24 22:22:37 +0300

    removed scroll effect

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,33 +6,31 @@ import userIcon from "@/assets/icons/user.svg";
 export default function About() {
     return (
         <section className=" bg-gradient-to-t from-black via-transparent to-black ">
-            <ScrollAnimationSection>
-                <div className="grid lg:grid-cols-2 lg:gap-0 gap-24 lg:py-0 py-32  min-h-screen">
-                    <div className="flex flex-col justify-center items-center p-4">
-                        <div className="grid gap-10 place-items-center lg:place-items-start">
-                            <Title image={userIcon}>About me</Title>
+            <div className="grid lg:grid-cols-2 lg:gap-0 gap-24 lg:py-0 py-32  min-h-screen">
+                <div className="flex flex-col justify-center items-center p-4">
+                    <div className="grid gap-10 place-items-center lg:place-items-start">
+                        <Title image={userIcon}>About me</Title>
 
-                            <div className="max-w-96 grid gap-3 2xl:text-xl text-lg lg:text-start text-center">
-                                <p>
-                                    A King Saud University Software Engineering
-                                    student passionate in Web development.
-                                </p>
-                                <p>
-                                    Awarded the Dean’s Award for Outstanding
-                                    Academic Excellence.
-                                </p>
-                                <p>
-                                    Developed numerous web applications
-                                    leveraging cutting-edge technologies.
-                                </p>
-                            </div>
+                        <div className="max-w-96 grid gap-3 2xl:text-xl text-lg lg:text-start text-center">
+                            <p>
+                                A King Saud University Software Engineering
+                                student passionate in Web development.
+                            </p>
+                            <p>
+                                Awarded the Dean’s Award for Outstanding
+                                Academic Excellence.
+                            </p>
+                            <p>
+                                Developed numerous web applications leveraging
+                                cutting-edge technologies.
+                            </p>
                         </div>
                     </div>
-                    <div className="grid place-items-center">
-                        <Stats />
-                    </div>
                 </div>
-            </ScrollAnimationSection>
+                <div className="grid place-items-center">
+                    <Stats />
+                </div>
+            </div>
         </section>
     );
 }
